76c6afd01ba20e5de6f11d828acc48686802f710
DESKTOP-90LV02M\admin <leducnamtek123@gmail.com>
2024-08-29 10:56:08 +0700

    no message

---
+++ Shared/Common/Configurations/JwtSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Configurations
+{
+    public class JwtSettings
+    {
+        public string ConnectionString { get; set; }
+        public string JwtSecret { get; set; }
+    }
+}
